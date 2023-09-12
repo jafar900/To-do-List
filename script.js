da48cc80bf2621612f1044f924e811a05d25f069
jafar900 <jafarmohammed900@gmail.com>
2023-09-12 16:13:47 +0530

    Adding button

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const collectitem = document.querySelector('#AddItem')
 const additem = document.querySelector('#to-do-list')
+const btn =document.querySelector('#btn')
+
+btn.addEventListener('click', 
+function(){
+    addTodo(collectitem.value);
+    collectitem.value=""
+}
 
 
-collectitem.addEventListener("keyup",(e)=>{
-      if(e.key == "Enter"){
-        console.log(collectitem.value)
-        addTodo(collectitem.value)
-        collectitem.value=""
-      }
-})
+)
+// collectitem.addEventListener("keyup",(e)=>{
+//       if(e.key == 'Enter'){
+//         console.log(btn)
+//         console.log(collectitem.value)
+//         addTodo(collectitem.value)
+//         collectitem.value=""
+//       }
+// })
 
 
 const addTodo = (item)=>{
